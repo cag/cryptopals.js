421787e9d305af72cc5974ae6e1884a849ca0a6e
Alan Lu <alanlu1023@gmail.com>
2017-08-24 18:17:16 -0500

    Fail

---
+++ test/set1.js
@@ -0,0 +1,29 @@
+const assert = require('assert')
+const Cryptopals = require('..')
+
+describe('Crypto Challenge Set 1', () => {
+    it('Convert hex to base64', () => {
+        assert.fail('not done yet')
+    })
+    it('Fixed XOR', () => {
+        assert.fail('not done yet')
+    })
+    it('Single-byte XOR cipher', () => {
+        assert.fail('not done yet')
+    })
+    it('Detect single-character XOR', () => {
+        assert.fail('not done yet')
+    })
+    it('Implement repeating-key XOR', () => {
+        assert.fail('not done yet')
+    })
+    it('Break repeating-key XOR', () => {
+        assert.fail('not done yet')
+    })
+    it('AES in ECB mode', () => {
+        assert.fail('not done yet')
+    })
+    it('Detect AES in ECB mode', () => {
+        assert.fail('not done yet')
+    })
+})
