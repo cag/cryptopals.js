df7cfae320c3931c691e07363fd7dcf54f6fea41
Alan Lu <alanlu1023@gmail.com>
2017-08-24 18:36:38 -0500

    Dat xor hex

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 module.exports = {
     hexToBase64(hexStr) {
         return Buffer.from(hexStr, 'hex').toString('base64')
-    }
+    },
+
+    xorHexes(a, b) {
+        const bBuf = Buffer.from(b, 'hex')
+        return Buffer.from(a, 'hex').map((val, i) => val ^ bBuf[i]).toString('hex')
+    },
 }
